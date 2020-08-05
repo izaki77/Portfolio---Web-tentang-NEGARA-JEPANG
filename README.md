85093ebb8fbee2ffeb21d9de64caddf039899397
izaki77 <fakriealfisahrin@gmail.com>
2020-08-06 01:07:33 +0700

    first-commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfolio---Web-tentang-NEGARA-JEPANG
---
+++ assets/web.js
@@ -0,0 +1,11 @@
+   alert("Halo Dicoding! :), Selamat datang! :)");
+
+	window.setTimeout("waktu()", 1000);
+
+	function waktu() {
+		var waktu = new Date();
+		setTimeout("waktu()", 1000);
+		document.getElementById("jam").innerHTML = waktu.getHours()+ " : ";
+		document.getElementById("menit").innerHTML = waktu.getMinutes()+ " : ";
+		document.getElementById("detik").innerHTML = waktu.getSeconds();
+	}
